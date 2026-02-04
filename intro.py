c02c06c5210357747e5266f65682240ded25239e
Henry <hbi7268@stacmail.net>
2026-02-05 12:04:23 +1300

    print statement on intro file

---
+++ intro.py
@@ -0,0 +1,7 @@
+'''this is the practice code'''
+#print is a function that outputs whatever is inside the brackets
+#numbers can be put in print
+print(67)
+
+#to put letters in print you need "" which turns it into a string
+print("hello")
