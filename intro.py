bc76dcb9ac2af2038a884e59de2b05c7445bf99a
Henry <hbi7268@stacmail.net>
2026-02-05 12:34:49 +1300

    trying to make it say hello

--- intro.py
+++ intro.py
@@ -1,7 +1,45 @@
 '''this is the practice code'''
-#print is a function that outputs whatever is inside the brackets
-#numbers can be put in print
+# print is a function that outputs whatever is inside the brackets
+# numbers can be put in print
+
 print(67)
 
-#to put letters in print you need "" which turns it into a string
+# to put letters in print you need "" which turns it into a string
+
 print("hello")
+
+# decimals = floating point numbers - float means decimal
+# boolean yes / no - true / false
+
+# we use variables to store information
+# varibles must all be lower case
+
+name = "margit"
+
+# to use multiple words for variable you go
+# first_name = "margit"
+# last_name = "omen"
+# age = 67
+
+age = 5000 
+
+# can print variables using ()
+# to combine variables with a string we use f-string
+# in a f-string the variable goes in the curly brakets {}
+
+print(f"my favourite video game character is {name} and he is {age} years old")
+
+# we can use input() to get input from the user
+# to make pernimant type user_name = input ("what is your name?")
+user_name = input ("what is your name tranished one?")
+print (f"{user_name}")
+
+print= input ("how old are you?")
+
+# print hello to the user and ask them how old they are
+
+margit = input("what is your name")
+
+print(f"hello {margit}")
+
+print(f"you are {margit} and you are {age} years old")
