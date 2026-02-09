cf312cc7ee36f6d0c75515a73d2196c80bd85502
Henry <hbi7268@stacmail.net>
2026-02-09 17:46:20 +1300

    The code you would need for asking for a blood test

---
+++ '''another practice'''.py
@@ -0,0 +1,20 @@
+'''another practice'''
+
+print("welcome to blood tests")
+
+CHILD_AGE = 16
+
+weight = 50
+
+age=int(input ("how old are you"))
+if age >= CHILD_AGE:
+    print("You are old enough for a blod test")
+else:
+    print("you are underage for blodtest")
+
+weight=int(input ("how much do you weigh"))
+
+if weight >= 50:
+    print("you are able to apply for a bloodtest")
+else:
+    print("you are underweight for blodtest please leave")
