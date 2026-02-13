3f4d6dc87a1d6869484f5fb64c69b57913edb5bc
Henry <hbi7268@stacmail.net>
2026-02-13 14:25:34 +1300

    todays work for loops and stuff

---
+++ fizz buzz.py
@@ -0,0 +1,13 @@
+'''fizz buzz'''
+
+#frizz buzz program
+for i in range(0,31):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
+
---
+++ for loop.py
@@ -0,0 +1,27 @@
+'''Deomonstration of for loop'''
+
+#makes it repeat for 10 times it goes from 0 to the number minus 1 
+'''
+for i in range(0,10):
+    print("Hello world")
+    print(i)
+
+for a in range(18,23):
+    print("Hi guys")
+    print(a)
+
+
+for b in range(0,3):
+    name = input("What is your name? ")
+    print(f"Hello {name}!")
+'''
+
+num = int(input("How many times do you want to repeat? "))
+
+
+for c in range(num):
+    print("What is your name?")
+    mane = input("What is your name?")
+    print(f"Hello {mane}!")
+
+#btw cause ima forget intinput thing is for numbers only
